34e35865fed8a409557ca0c19ad1c1f02416b8ad
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-04-05 09:17:31 +0300

    Add WebDriverWait to the FirstTest_Copy_Chupin.java

---
+++ src/test/java/Aviasales.java
@@ -0,0 +1,71 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Aviasales {
+    WebDriver webDriver;
+
+    @BeforeTest
+    public void beforeTest(){
+        // String pathToDriver = "D:/Программы/chromedriver_win32.chromedriver.exe";
+        // System.setProperty("webdriver.chrome.driver", pathToDriver);
+        WebDriverManager.chromedriver().setup();;
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+    }
+
+    @Test(invocationCount = 1)
+    public void  firstTest(){
+        webDriver.get("https://www.aviasales.by/");
+        WebElement inputField = webDriver.findElement(By.name("q"));
+        inputField.sendKeys("Котики");
+        //*[@id="origin"]
+        //*[@id="destination"]
+        
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        inputField.sendKeys(Keys.ENTER);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String header = webDriver.getTitle();
+        Assert.assertEquals(header, "Котики - Поиск в Google");
+
+        List<WebElement> listElements = webDriver.findElements(By.xpath("//h3"));
+        for (WebElement el: listElements) {
+            System.out.println(el.getText());
+        }
+        Assert.assertFalse(listElements.size()==20);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @AfterTest
+    public void afterTest(){
+        webDriver.quit();
+    }
+
+}
--- src/test/java/FirstTest_Copy_Chupin.java
+++ src/test/java/FirstTest_Copy_Chupin.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -20,30 +21,29 @@ public class FirstTest_Copy_Chupin {
     public void beforeTest(){
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
-        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
     }
-
     @Test
     public void firstTest(){
         webDriver.get("https://google.com");
-        // WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofMillis(180));
-      //  WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("input1")));
+        WebDriverWait wait = new WebDriverWait(webDriver, 180);
+        WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("input")));
 
-        //WebElement inputField = webDriver.findElement(By.tagName("input1"));
-       // inputField.sendKeys("Котики");
+        // WebElement inputField = webDriver.findElement(By.tagName("input1"));
+        inputField.sendKeys("Котики");
 
-       // inputField.sendKeys(Keys.ENTER);
+        inputField.sendKeys(Keys.ENTER);
 
         String header = webDriver.getTitle();
         Assert.assertEquals(header, "Котики - Поиск в Google");
 
         List<WebElement> listElements = webDriver.findElements(By.xpath("//h3"));
-        Assert.assertTrue(listElements.size()==10);
+        System.out.println(listElements.size());
+        Assert.assertTrue(listElements.size()==11);
     }
 
     @AfterTest
     public void afterTest() {
         webDriver.quit();
     }
-
 }
