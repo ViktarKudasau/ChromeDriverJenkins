a18d93174722dbe79a7338d2c513bbf781b6680d
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-04-11 13:28:43 +0300

    Delete escape from the BasePage and from the BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import pageOblects.MainPage;
 import pageOblects.RubberDucksPage;
-
 import java.time.Duration;
 
 public class BaseTest {
--- src/test/java/pageOblects/BasePage.java
+++ src/test/java/pageOblects/BasePage.java
@@ -2,7 +2,6 @@ package pageOblects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class BasePage {
