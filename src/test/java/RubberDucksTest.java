1e77e3901bb5f11a88405faa367f173ba7766939
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-04-11 12:54:03 +0300

    Add sortByDateTest to the RubberDucksTest

--- src/test/java/RubberDucksTest.java
+++ src/test/java/RubberDucksTest.java
@@ -5,14 +5,18 @@ public class RubberDucksTest extends BaseTest {
 
     @Test
     public void quantityDucksTest() {
-        rubberDucksPage.verifyLoadButtonRubberDucks();
-        rubberDucksPage.clickButtonRubberDucks();
-        rubberDucksPage.verifyLoadDucks();
+        rubberDucksPage.clickRubberDucks();
         Assert.assertEquals(rubberDucksPage.ListSize(), rubberDucksPage.expected_Quantity_Ducks);
         System.out.println("Quantity of Ducks = " + rubberDucksPage.ListSize());
     }
-    @Test
-    public void sortByDateTest(){
 
+    @Test
+    public void sortByDateTest() {
+        rubberDucksPage.clickRubberDucks();
+        rubberDucksPage.clickDate();
+        Assert.assertEquals(webDriver.findElement(rubberDucksPage.price_first_duck_ByDate).getText(),
+                rubberDucksPage.Expected_price_by_Date);
+        System.out.println("Price first duck by sort of date = " +
+                webDriver.findElement(rubberDucksPage.price_first_duck_ByDate).getText());
     }
 }
--- src/test/java/pageOblects/RubberDucksPage.java
+++ src/test/java/pageOblects/RubberDucksPage.java
@@ -12,6 +12,8 @@ public class RubberDucksPage extends BasePage {
     private final By Button_Date = By.xpath("//a[text()='Date']");
     private final By Ducks = By.xpath("//li[@class='product column shadow hover-light']");
     public int expected_Quantity_Ducks = 5;
+    public By price_first_duck_ByDate = By.xpath("//a[@title='Yellow Duck']/div//strong");
+    public String Expected_price_by_Date = "$18";
 
     public RubberDucksPage(WebDriver driver) {
         super(driver);
@@ -21,7 +23,7 @@ public class RubberDucksPage extends BasePage {
         webDriver.findElement(Button_Rubber_Ducks).click();
     }
     public void clickButtonDate() {
-        webDriver.findElement(Button_Rubber_Ducks).click();
+        webDriver.findElement(Button_Date).click();
     }
 
     public int ListSize() {
@@ -32,10 +34,22 @@ public class RubberDucksPage extends BasePage {
     public void verifyLoadButtonRubberDucks() {
         wait.until(ExpectedConditions.presenceOfElementLocated(Button_Rubber_Ducks));
     }
-    public void verifyLoadSortByDate() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(Button_Rubber_Ducks));
+    public void verifyLoadButtonDate() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(Button_Date));
     }
     public void verifyLoadDucks() {
         wait.until(ExpectedConditions.presenceOfElementLocated(Ducks));
+    }
+
+    public void clickRubberDucks(){
+        verifyLoadButtonRubberDucks();
+        clickButtonRubberDucks();
+        verifyLoadDucks();
+    }
+
+    public void clickDate(){
+        verifyLoadButtonDate();
+        clickButtonDate();
+        verifyLoadDucks();
     }
 }
