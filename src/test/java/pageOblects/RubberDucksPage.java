d5e25499f27a329cf88d21867512a9452a5263f1
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-04-13 13:38:12 +0300

    Add Selenide to the RubberDucksPage

--- src/test/java/pageOblects/RubberDucksPage.java
+++ src/test/java/pageOblects/RubberDucksPage.java
@@ -1,68 +1,52 @@
 package pageOblects;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import java.util.List;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class RubberDucksPage extends BasePage {
-    private final By Button_Rubber_Ducks = By.xpath("//a[text()='Rubber Ducks']");
-    private final By Button_Date = By.xpath("//a[text()='Date']");
-    private final By Button_Name = By.xpath("//a[text()='Name']");
-    private final By Ducks = By.xpath("//li[@class='product column shadow hover-light']");
-    private final By price_first_duck_By_Date = By.xpath("//a[@title='Yellow Duck']/div//strong");
-    private final By price_first_duck_By_Name = By.xpath("//a[@title='Blue Duck']/div/span");
+    private static final By Button_Rubber_Ducks = By.xpath("//a[text()='Rubber Ducks']");
+    private static final By Button_Date = By.xpath("//a[text()='Date']");
+    private static final By Button_Name = By.xpath("//a[text()='Name']");
+    private static final By Ducks = By.xpath("//li[@class='product column shadow hover-light']");
+    private static final By price_first_duck_By_Date = By.xpath("//a[@title='Yellow Duck']/div//strong");
+    private static final By price_first_duck_By_Name = By.xpath("//a[@title='Blue Duck']/div/span");
 
     public RubberDucksPage(WebDriver driver) {
         super(driver);
     }
 
-    public void clickButtonRubberDucks() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(Button_Rubber_Ducks));
-        webDriver.findElement(Button_Rubber_Ducks).click();
+    public void verifyLoadDuck(){
+        $(Ducks).shouldBe(Condition.visible);
     }
-
-    public void clickButtonDate() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(Button_Date));
-        webDriver.findElement(Button_Date).click();
-    }
-
-    public void clickButtonName() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(Button_Name));
-        webDriver.findElement(Button_Name).click();
-    }
-
-    public void verifyLoadDucks() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(Ducks));
-    }
-
     public void clickRubberDucks() {
-        clickButtonRubberDucks();
-        verifyLoadDucks();
+        $(Button_Rubber_Ducks).shouldBe(Condition.exist).click();
+        verifyLoadDuck();
     }
 
     public void clickDate() {
-        clickButtonDate();
-        verifyLoadDucks();
+        $(Button_Date).shouldBe(Condition.exist).click();
+        verifyLoadDuck();
     }
 
     public void clickName() {
-        clickButtonName();
-        verifyLoadDucks();
+        $(Button_Name).shouldBe(Condition.exist).click();
+        verifyLoadDuck();
     }
 
     public int getQuantityDucks() {
-        List<WebElement> listElements = webDriver.findElements(Ducks);
+        ElementsCollection listElements = $$(Ducks);
         return listElements.size();
     }
 
     public String getPriceByName() {
-        return webDriver.findElement(price_first_duck_By_Name).getText();
+        return $(price_first_duck_By_Name).shouldBe(Condition.exist).getText();
     }
 
     public String getPriceByDate() {
-        return webDriver.findElement(price_first_duck_By_Date).getText();
+        return $(price_first_duck_By_Date).shouldBe(Condition.exist).getText();
     }
 }
